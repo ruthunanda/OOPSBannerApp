8a8263db29595e16c60e9c7a6067de571db9feaf
ruthunanda <ruthunandaanil110407@gmail.com>
2026-02-17 11:42:10 +0530

    Add files via upload

---
+++ OOPSBannerAppwithjoin.java
@@ -0,0 +1,16 @@
+public class OOPSBannerAppwithjoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   *        *      **        *** "));
+        System.out.println(String.join(" "," *   *    *   *    *    *     **     "));
+        System.out.println(String.join(" ","*     *  *     *   *     *   **     "));
+        System.out.println(String.join(" ","*     *  *     *   *    *     **  "));
+        System.out.println(String.join(" ","*     *  *     *   **        *"));
+        System.out.println(String.join(" ","*     *  *     *   *              *  "));
+        System.out.println(String.join(" ","*     *  *     *   *               *     "));
+        System.out.println(String.join(" "," *   *    *    *   *              *       "));
+        System.out.println(String.join(" ","   *        *      *         **    "));
+
+    }
+}
