3eaa5a1a0a1a272ec64e35a8c4786ab01186ecf8
ruthunanda <ruthunandaanil110407@gmail.com>
2026-02-17 11:37:03 +0530

    Add files via upload

---
+++ OOPSBannerAppwithstring.java
@@ -0,0 +1,16 @@
+public class OOPSBannerAppwithstring {
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***        ***      ******        ***** ");
+        System.out.println(" **   **    **   **    **    **     **     ");
+        System.out.println("**     **  **     **   **     **   **     ");
+        System.out.println("**     **  **     **   **    **     **  ");
+        System.out.println("**     **  **     **   ******        ***");
+        System.out.println("**     **  **     **   **              **  ");
+        System.out.println("**     **  **     **   **               **     ");
+        System.out.println(" **   **    **    **   **              **       ");
+        System.out.println("   ***        ***      **         *****    ");
+
+    }
+}
